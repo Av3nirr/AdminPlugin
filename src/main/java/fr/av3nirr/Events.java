863e35b0aaa0ade80ba9edf371fba015344c52ec
Av3nirr <caillettheo1@gmail.com>
2022-10-21 19:01:23 +0200

    Resolved some bugs

--- src/main/java/fr/av3nirr/Events.java
+++ src/main/java/fr/av3nirr/Events.java
@@ -1,19 +1,44 @@
 package fr.av3nirr;
 
 import net.kyori.adventure.title.Title;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractAtEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.player.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 import java.util.Objects;
 
 public class Events implements Listener {
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent e){
+        Player p = e.getPlayer();
+        if (Main.getInstance().Admins.contains(p)){
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onDrop(PlayerDropItemEvent e) {
+        Player p = e.getPlayer();
+        if (Main.getInstance().Admins.contains(p)){
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public  void onPearl(ProjectileLaunchEvent e){
+        Player p = (Player) e.getEntity().getShooter();
+        if (Main.getInstance().Admins.contains(p)){
+            e.setCancelled(true);
+        }
+    }
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e){
         Player player = e.getPlayer();
@@ -27,20 +52,20 @@ public class Events implements Listener {
         Player p = e.getPlayer();
         if(Main.getInstance().freezedPlayers.contains(p)){
             e.setCancelled(true);
-            p.sendMessage("§cTu ne peux pas bouger !");
-            p.sendTitle("§Tu es Freeze !", "§fVa sur le discord: discord", 1, 10, 1);
+            p.sendTitle("§bTu es Freeze !", "§fVa sur le discord: discord", 1, 10, 1);
         }
     }
     @EventHandler
     public  void onRightClick(PlayerInteractAtEntityEvent e){
         Player p = e.getPlayer();
         Player target = (Player) e.getRightClicked();
+        Player freezeTarget = (Player) p.getServer().getOfflinePlayer(target.getUniqueId());
         //La condition ne s'enclanche pas avec un ==, il faut tester avec un .equals()
         if (Objects.equals(p.getItemInHand(), Main.getInstance().kickItem)) {
             target.kick();
             p.sendMessage("§aVous avez correctement kick §r" + target.getName() + " §adu serveur !");
-        }else if(Objects.equals(p.getItemInHand(), Main.getInstance().vanishItem)){
-            Main.getInstance().freezedPlayers.add(target);
+        }else if(Objects.equals(p.getItemInHand(), Main.getInstance().freezeItem)){
+            Main.getInstance().freezedPlayers.add(freezeTarget);
             target.sendMessage("§bVous venez d'être Freeze par §e" + p.getName());
             p.sendMessage("§aVous venez de Freeze §e" + target.getName());
         }
@@ -74,20 +99,20 @@ public class Events implements Listener {
             }
         } else if (Objects.equals(p.getItemInHand(), Main.getInstance().flyItem)) {
             if(Main.getInstance().FlyingPlayers.contains(p)){
+                Main.getInstance().FlyingPlayers.remove(p);
                 p.setFlying(false);
                 p.setAllowFlight(false);
-                Main.getInstance().FlyingPlayers.remove(p);
                 p.sendMessage("§cFly désactivé !");
             }else{
-                p.setAllowFlight(true);
                 Main.getInstance().FlyingPlayers.add(p);
+                p.setAllowFlight(true);
                 p.sendMessage("§aFly activé !");
             }
         } else if (Objects.equals(p.getItemInHand(), Main.getInstance().randomTpItem)){
             int nombreAleatoire = (int) (Math.random() * ((p.getServer().getOnlinePlayers().size()) + 1));
             Player target = (Player) p.getServer().getOnlinePlayers().toArray()[nombreAleatoire];
             p.teleport(target);
-            p.sendMessage("§cTu as été téléporté au joueur §f" + p.getName());
+            p.sendMessage("§cTu as été téléporté au joueur §f" + target.getName());
         }
     }
 }
